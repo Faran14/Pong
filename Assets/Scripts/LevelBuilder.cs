c0419b6072443250a8c5b9913743a2c3a27fa4f9
Faran Tauseef <faran.tauseef@mindstormstudios.com>
2022-08-15 00:12:46 +0500

    Implemented Levelbuilder

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class LevelBuilder : MonoBehaviour
 {
     
-    private float _width;
+    public static float _width;
     public GameObject WallRight;
     public GameObject WallLeft;
     public GameObject GoalTop;
     public GameObject GoalBottom;
+    public GameObject PlayerPaddle;
+    public GameObject AIPaddle;
     // Start is called before the first frame update
     
     private void Start()
@@ -17,11 +19,15 @@ public class LevelBuilder : MonoBehaviour
         GetAspectRatio();
         WallSpawner();
         GoalSpawner();
+        P1Spawner();
+        P2Spawner();
+
     }
 
     private void GetAspectRatio()
     {
         _width = Camera.main.aspect * 10;
+        Debug.Log(_width);
     }
     private void WallSpawner()
     {
@@ -34,9 +40,21 @@ public class LevelBuilder : MonoBehaviour
     private void GoalSpawner()
     {
         GoalTop.transform.position = new Vector2(0, 5);
-        GoalTop.transform.localScale = new Vector2(_width, 0.5f);
+        GoalTop.transform.localScale = new Vector2(_width, 0.05f);
         GoalBottom.transform.position = new Vector2(0, -5);
-        GoalBottom.transform.localScale = new Vector2(_width, 0.5f);
+        GoalBottom.transform.localScale = new Vector2(_width, 0.05f);
     }
+
+    private void P1Spawner()
+    {
+        PlayerPaddle.transform.position = new Vector2(0, -4.9f);
+        PlayerPaddle.transform.localScale = new Vector2(_width/3,0.25f);
+    }
+    private void P2Spawner()
+    {
+        AIPaddle.transform.position = new Vector2(0, 4.9f);
+        AIPaddle.transform.localScale = new Vector2(_width / 3, 0.25f);
+    }
+
 
 }
